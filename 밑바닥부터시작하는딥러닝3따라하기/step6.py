f2a2cd68da739af8eeb4911fde7a0c39b9ca193f
yunjinchoidev <yunjinchoidev@gmail.com>
2023-03-01 14:20:41 +0900

    step5,6

---
+++ 밑바닥부터시작하는딥러닝3따라하기/step6.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+class Variable:
+    def __init__(self, data):
+        self.data = data
+        self.grad = None
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        self.input = input # 입력값을 보관하자.
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+    
+    def backward(self, gy):  # 역전파에 쓰일함수
+        raise NotImplementedError()
+    
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    ## 역전파 함수가 왜 이렇게 생성되는 지 설명할 수 있는가? 
+    def backward(slef, gy):
+        x = self.input.data
+        gx = 2*x*gy
+        return gx
+    
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+    
+    ## 역전파 함수가 왜 이렇게 생성되는 지 설명할 수 있는가?     
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x)*gy
+        return gx
+
+
+
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+print(x.grad)
